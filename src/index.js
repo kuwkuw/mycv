2babbe53dbd88a346bf2d9177c59f7ebcd5de2d8
Oleg Kushch <oleg.kushch85@gmail.com>
2020-02-12 14:49:20 +0200

    Add raw personal info generation

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import './styles/main.scss';
-import headerTemplate from './templates/header.hbs';
-import mainTemplate from './templates/main.hbs'; 
+import "./styles/main.scss";
 
+import mainTemplate from "./templates/main.hbs";
 
-fetch('/data/cv-data.json').then(data => {
-    const rootElement = document.querySelector('body>.container');
-    data.json().then(cvData => {
-        addPersonalInfo(rootElement, cvData.personal);
-    });
+fetch("/data/cv-data.json").then(data => {
+  const rootElement = document.querySelector("body>.container");
+  data.json().then(cvData => {
+    addPersonalInfo(rootElement, cvData);
+  });
 });
 
-function addHeaderView() {
-    return headerTemplate();
-}
-
-function addPersonalInfo(cotainerElement, personalInformation) {
-    const headerElement = cotainerElement.querySelector('header');
-    const mainElemnt = cotainerElement.querySelector('main');
-    headerElement.innerHTML = addHeaderView();
-    mainElemnt.innerHTML = mainTemplate();
+function addPersonalInfo(containerElement, personalInformation) {
+  containerElement.innerHTML = mainTemplate({
+    headerData: personalInformation.personal,
+    sidebarData: {
+      education: personalInformation.education,
+      skills: personalInformation.skills
+    },
+    mainContentData: {
+      profile: personalInformation.profile,
+      experience: personalInformation.experience
+    }
+  });
 }
